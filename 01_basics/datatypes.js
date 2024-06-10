6935624b898c1869133da5d57c94f84303ab4e5a
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-06-10 08:00:50 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strick"; // treat all js code as newer version
+
+//alert(3+3) we are using node.js, not browser
+//{premitive datatypes}
+//number => 2 to power 53
+// bigint
+//string = ""
+//boolean = true/false 
+//null => standalone value
+//undefined => 
+//symbol => 
+
+    //object
+     console.log(typeof null) //null is a type of object 
+     console.log(typeof undefined) // undefiend is type
