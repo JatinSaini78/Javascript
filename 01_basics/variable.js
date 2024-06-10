6ff342f05f1c19309acd5ca5878f32cec95a86d3
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-06-10 08:01:09 +0000

    variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "Jatin@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+//console.log(accountId);
