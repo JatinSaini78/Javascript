374314f9762bc8206510a45a7f32b171b8b25df4
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-06-12 10:39:51 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,15 +1,19 @@
-"use strick"; // treat all js code as newer version
+// nn bb ss u primitive in js
+let a = null;
+let b = 345;
+let c = true // can also be false
+let d = BigInt("234")
+let e = "Jatin"
+let f = Symbol("this is a symbol")
+let g = undefined 
 
-//alert(3+3) we are using node.js, not browser
-//{premitive datatypes}
-//number => 2 to power 53
-// bigint
-//string = ""
-//boolean = true/false 
-//null => standalone value
-//undefined => 
-//symbol => 
+console.log(a, b, c, d, e, f, g)
 
-    //object
-     console.log(typeof null) //null is a type of object 
-     console.log(typeof undefined) // undefiend is type
+// non primitive datatypes object in js
+ 
+const item = {
+    "Jatin" : true,
+    "Shubh" : false,
+    "Lovish" : 67
+}
+console.log(item["Jatin"])
