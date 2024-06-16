5f2e0ff3c6ba95821de316e8fbcb068813dbc5b0
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-06-16 07:23:51 +0000

    practice

---
+++ 01_basics/practice.js
@@ -0,0 +1,21 @@
+let a = "Jatin"
+let b = 6
+console.log(a+b)
+
+let c = "Jatin"
+console.log(typeof (a+b))
+
+const a1 = {
+    name: "Jatin",
+    section: 1,
+    isPrinciple: false 
+}
+//a1 = 12
+a1['friend'] = "Saini"
+ console.log(a1)
+
+ const dict = {
+ 
+ }
+
+ 
