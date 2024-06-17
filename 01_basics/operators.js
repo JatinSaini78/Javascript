6fac6e7d02a75ed1a4f7c8ff67f32eca208a1461
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-06-17 17:31:22 +0000

    operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,24 @@
+let a = 15
+let b = 10
+//arithmatic operator
+//console.log("a + b = ", a*b)
+//console.log("a-b = ", a-b)
+//console.log("a / b = ", a/b)
+//console.log("a * b = ", a*b)
+//console.log("a + b = ", a*b) value= 16, 16, 15, 15, 
+//console.log("a + b = ", a*b)
+//console.log("++a = ", ++a) 
+//console.log("a++ = ", a++)
+//console.log("--a = ", --a)
+//console.log("a-- = ", a--)
+//console.log("a = ", a)
+//console.log("a-- = ", a--)
+
+//assignment operators
+let c = 1
+c += 5
+console.log("c is now =", c)
+c -= 5
+console.log("c is now =", c)
+c *= 5
+console.log("c is now =", c)
